a9e5ee8a559e161b91d9701338f4f09b526e98ee
C-H-F <git@hyparvisor.com>
2024-01-28 21:51:40 +0000

    Backend: Fix network interface type parsing.
    (cherry picked from commit ae78928a2ad343f9b0611afdb2930c25a46d69ef)

--- application/backend/src/models/networkInterface.ts
+++ application/backend/src/models/networkInterface.ts
@@ -101,9 +101,9 @@ export function networkInterfaceFromXml(
     mutXmlData.model
   ) {
     if ('@type' in mutXmlData.model) {
-      result.model = networkInterfaceDevice
-        .pick({ model: true })
-        .parse(mutXmlData.model['@type']).model;
+      result.model = networkInterfaceDevice.shape.model.parse(
+        mutXmlData.model['@type']
+      );
       delete mutXmlData.model['@type'];
     }
     if (isEmptyObject(mutXmlData.model)) delete mutXmlData.model;
