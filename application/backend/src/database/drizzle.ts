bca2941c82c775f56d65eda7123de4f384d8bdd0
C-H-F <git@hyparvisor.com>
2024-02-23 23:12:04 +0100

    Backend: Add initial SQL script

--- application/backend/src/database/drizzle.ts
+++ application/backend/src/database/drizzle.ts
@@ -1,5 +1,17 @@
-import { Database } from 'bun:sqlite';
+import Database from '@farjs/better-sqlite3-wrapper';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
+import databaseSetup from '../../_drizzle/sql.txt';
 
 const sqlite = new Database('hyparvisor.db');
 export const db = drizzle(sqlite);
+
+//Setup database
+if (
+  !sqlite
+    .prepare(
+      `SELECT COUNT(*) as cnt FROM sqlite_master WHERE type='table' AND name='user'`
+    )
+    .get().cnt
+) {
+  sqlite.prepare(databaseSetup).run();
+}
