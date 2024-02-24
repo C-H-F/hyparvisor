8fe742505e03ae3dd8b4a904a3845ac348cb1ccf
C-H-F <git@hyparvisor.com>
2024-02-24 20:09:47 +0100

    Backend: Initialize database after installation.
    Use better-sqlite3-wrapper.

--- application/backend/src/database/drizzle.ts
+++ application/backend/src/database/drizzle.ts
@@ -1,5 +1,20 @@
-import { Database } from 'bun:sqlite';
+import Database from '@farjs/better-sqlite3-wrapper';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
+import databaseSetup from '../../database/0000.sql.txt';
 
 const sqlite = new Database('hyparvisor.db');
 export const db = drizzle(sqlite);
+
+//Setup database
+if (
+  !sqlite
+    .prepare(
+      `SELECT COUNT(*) as cnt FROM sqlite_master WHERE type='table' AND name='user'`
+    )
+    .get().cnt
+) {
+  databaseSetup.split('--> statement-breakpoint').forEach((sql) => {
+    if (sql.trim() === '') return;
+    sqlite.prepare(sql).run();
+  });
+}
