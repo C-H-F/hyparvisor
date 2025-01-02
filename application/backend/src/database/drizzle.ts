6767b7a75e7d9d4e12dc2e78748e803806754e19
C-H-F <git@hyparvisor.com>
2025-01-03 00:27:27 +0100

    Backend: Update database initialization.
    No backwards compatibility needed. We can just replace the old database by deleting it.

--- application/backend/src/database/drizzle.ts
+++ application/backend/src/database/drizzle.ts
@@ -1,6 +1,7 @@
 import Database from '@farjs/better-sqlite3-wrapper';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
-import databaseSetup from '../../database/0000.sql.txt';
+import databaseSetup0000 from '../../database/0000.sql.txt';
+import databaseSetup0001 from '../../database/0001.sql.txt';
 
 const sqlite = new Database('hyparvisor.db');
 export const db = drizzle(sqlite as any);
@@ -13,8 +14,9 @@ if (
     )
     .get().cnt
 ) {
-  databaseSetup.split('--> statement-breakpoint').forEach((sql) => {
-    if (sql.trim() === '') return;
-    sqlite.prepare(sql).run();
-  });
+  for (const databaseSetup of [databaseSetup0000, databaseSetup0001])
+    databaseSetup.split('--> statement-breakpoint').forEach((sql) => {
+      if (sql.trim() === '') return;
+      sqlite.prepare(sql).run();
+    });
 }
